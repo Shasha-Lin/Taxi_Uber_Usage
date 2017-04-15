8acbc75344089e204aa15b28125175324149a676
Caroline Roper <ceroper@gmail.com>
2017-04-15 11:53:00 -0400

    Fixed bug in route heatmap

--- by_location.py
+++ by_location.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     counts_g = lines_g.filter(lambda x: x[1] != 'lpep_pickup_datetime').map(lambda x: ((x[5],x[6]),1)).reduceByKey(add).map(lambda x: str(x[0][0]) + '\t' + str(x[0][1]) + '\t' + str(x[1]))
 
     lines_y = sc.textFile('new_schema/yellow_*.csv')
+    lines_y = lines_y.mapPartitions(lambda x: reader(x)).filter(lambda x: len(x) >= 1)
 
     counts_y = lines_y.filter(lambda x: x[1] != 'tpep_pickup_datetime').map(lambda x: ((x[7],x[8]),1)).reduceByKey(add).map(lambda x: str(x[0][0]) + '\t' + str(x[0][1]) + '\t' + str(x[1]))
 
