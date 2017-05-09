1c9d130a9238e4c06e63052eebb4443cc05d2439
Caroline Roper <ceroper@gmail.com>
2017-05-09 16:22:38 -0400

    Final validation script and shell scripts

--- validation/validation_combined.py
+++ validation/validation_combined.py
@@ -28,50 +28,58 @@ if __name__ == "__main__":
     'fare_amount', 'extra', 'mta_tax', 'payment_type',  'store_and_fwd_flag', 
     'tip_amount', 'tolls_amount', 'improvement_surcharge', 'total_amount', 'ehail_fee', 'trip_type']
 
-    old_green= {'VendorID': 0, 'lpep_pickup_datetime:':1, 'lpep_dropoff_datetime':2, 'pickup_longitude': 5, 'pickup_latitude': 6, 'dropoff_longitude': 7, 'dropoff_latitude': 8, 
+    old_green= {'vendorid': 0, 'lpep_pickup_datetime:':1, 'lpep_dropoff_datetime':2, 'pickup_longitude': 5, 'pickup_latitude': 6, 'dropoff_longitude': 7, 'dropoff_latitude': 8, 
             'passenger_count': 9, 'trip_distance': 10, 'fare_amount': 11, 'extra': 12, 'mta_tax': 13, 'tip_amount': 14, 'tolls_amount':15, 'ehail_fee':16, 
-            'improvement_surcharge':17, 'total_amount': 18, 'payment_type': 19, 'trip_type':20, 'RateCodeID': 4, 'store_and_fwd_flag': 3}
+            'improvement_surcharge':17, 'total_amount': 18, 'payment_type': 19, 'trip_type':20, 'ratecodeid': 4, 'store_and_fwd_flag': 3}
 
-    new_green = {'VendorID': 0, 'lpep_pickup_datetime': 1, 'lpep_dropoff_datetime': 2, 'store_and_fwd_flag':3, 
-            'RatecodeID':4, 'PULocationID':5, 'DOLocationID':6, 'passenger_count':7, 'trip_distance':8, 'fare_amount':9, 
+    new_green = {'vendorid': 0, 'lpep_pickup_datetime': 1, 'lpep_dropoff_datetime': 2, 'store_and_fwd_flag':3, 
+            'ratecodeid':4, 'pulocationid':5, 'dolocationid':6, 'passenger_count':7, 'trip_distance':8, 'fare_amount':9, 
             'extra':10, 'mta_tax': 11, 'tip_amount': 12, 'tolls_amount': 13, 'ehail_fee': 14, 'improvement_surcharge':15, 
             'total_amount': 16, 'payment_type':17, 'trip_type': 18}
 
-    old_yellow = {'VendorID':0, 'tpep_pickup_datetime':1, 'tpep_dropoff_datetime':2, 'passenger_count':3, 
-            'trip_distance':4, 'pickup_longitude':5,'pickup_latitude':6,'RateCodeID':7,'store_and_fwd_flag':8,'dropoff_longitude':9, 
+    old_yellow = {'vendorid':0, 'tpep_pickup_datetime':1, 'tpep_dropoff_datetime':2, 'passenger_count':3, 
+            'trip_distance':4, 'pickup_longitude':5,'pickup_latitude':6,'ratecodeid':7,'store_and_fwd_flag':8,'dropoff_longitude':9, 
             'dropoff_latitude':10,'payment_type':11,'fare_amount':12,'extra':13,'mta_tax':14,'tip_amount':15,'tolls_amount':16, 
              'improvement_surcharge':17, 'total_amount':18}
 
-    new_yellow = {'VendorID':0, 'tpep_pickup_datetime':1,'tpep_dropoff_datetime':2,'passenger_count':3, 
-            'trip_distance':4,'RatecodeID':5,'store_and_fwd_flag':6,'PULocationID':7,'DOLocationID':8,'payment_type':9, 
+    new_yellow = {'vendorid':0, 'tpep_pickup_datetime':1,'tpep_dropoff_datetime':2,'passenger_count':3, 
+            'trip_distance':4,'ratecodeid':5,'store_and_fwd_flag':6,'pulocationid':7,'dolocationid':8,'payment_type':9, 
             'fare_amount':10,'extra':11,'mta_tax':12,'tip_amount':13,'tolls_amount':14,'improvement_surcharge':15, 'total_amount':16}
 
     missing = ['0', 'NULL', 'NaN', '', 'NAN', 'nan', 'None', 'none', 'Unknown', 'unknown']
     ratecodeid_missing = ['99']
 
     def pulocationid(value):
-        if 'LocationID' in semantic_type(value[0]):
-            if int(value[0]) in (264, 265):
-                value[-1] = 'MISSING'
-            else:
+        try:
+            LocationID = int(value[0])
+            if LocationID in (264, 265):
+                value[-1] = 'NULL'
+            elif LocationID in range(1,264):
                 value[-1] = 'VALID'
-        elif value[0] in missing:
-            value[-1] = 'MISSING'
-        else:
-            value[-1] = 'INVALID\OUTLIER'
+            else:
+                value[-1] = 'INVALID'
+        except ValueError:
+            if value[0] in missing:
+                value[-1] = 'NULL'
+            else:
+                value[-1] = 'INVALID'
         return value
 
 
     def dolocationid(value):
-        if 'LocationID' in semantic_type(value[0]):
-            if int(value[0]) in (264, 265):
-                value[-1] = 'MISSING'
-            else:
+        try:
+            LocationID = int(value[0])
+            if LocationID in (264, 265):
+                value[-1] = 'NULL'
+            elif LocationID in range(1,264):
                 value[-1] = 'VALID'
-        elif value[0] in missing:
-            value[-1] = 'MISSING'
-        else:
-            value[-1] = 'INVALID\OUTLIER'
+            else:
+                value[-1] = 'INVALID'
+        except ValueError:
+            if value[0] in missing:
+                value[-1] = 'NULL'
+            else:
+                value[-1] = 'INVALID'
         return value
 
     def lpep_pickup_datetime(value):
@@ -80,12 +88,12 @@ if __name__ == "__main__":
             if value_parsed.year in (2015, 2016):
                 value[-1] = 'VALID'
             else:
-                value[-1] = 'INVALID\OUTLIER'
+                value[-1] = 'INVALID'
         except ValueError:
             if value[0] in missing:
-                value[-1] = 'MISSING'
+                value[-1] = 'NULL'
             else:
-                value[-1] = 'INVALID\OUTLIER'
+                value[-1] = 'INVALID'
         return value
 
     def tpep_pickup_datetime(value):
@@ -94,12 +102,12 @@ if __name__ == "__main__":
             if value_parsed.year in (2015, 2016):
                 value[-1] = 'VALID'
             else:
-                value[-1] = 'INVALID\OUTLIER'
+                value[-1] = 'INVALID'
         except ValueError:
             if value[0] in missing:
-                value[-1] = 'MISSING'
+                value[-1] = 'NULL'
             else:
-                value[-1] = 'INVALID\OUTLIER'
+                value[-1] = 'INVALID'
         return value
 
     def lpep_dropoff_datetime(value):
@@ -108,12 +116,15 @@ if __name__ == "__main__":
             if value_parsed.year in (2015, 2016):
                 value[-1] = 'VALID'
             else:
-                value[-1] = 'INVALID\OUTLIER'
+                if value_parsed.month == 1 and value_parsed.day == 1:
+                    value[-1] = 'VALID'
+                else:
+                    value[-1] = 'INVALID'
         except ValueError:
             if date[0] in missing:
-                value[-1] = 'MISSING'
+                value[-1] = 'NULL'
             else:
-                value[-1] = 'INVALID\OUTLIER'
+                value[-1] = 'INVALID'
         return value
 
     def tpep_dropoff_datetime(value):
@@ -122,27 +133,33 @@ if __name__ == "__main__":
             if value_parsed.year in (2015, 2016):
                 value[-1] = 'VALID'
             else:
-                value[-1] = 'INVALID\OUTLIER'
+                if value_parsed.month == 1 and value_parsed.day == 1:
+                    value[-1] = 'VALID'
+                else:
+                    value[-1] = 'INVALID'
         except ValueError:
-            if value[0] in missing:
-                value[-1] = 'MISSING'
+            if date[0] in missing:
+                value[-1] = 'NULL'
             else:
-                value[-1] = 'INVALID\OUTLIER'
+                value[-1] = 'INVALID'
         return value
 
     def trip_distance(value):
-        if 'Distance' in semantic_type(value[0]):
-            if float(value[0]) < 100 and float(value[0]) > 0:
+        try:
+            distance = float(value[0])
+            if distance < 75 and distance > 0:
                 value[-1] = 'VALID'
-            elif float(value[0]) == 0:
-                value[-1] = 'MISSING'
-            elif 100 < float(value[0]):
-                value[-1] = 'INVALID\OUTLIER'
-        else:
-            if value in missing:
-                value[-1] = 'MISSING'
+            elif distance == 0:
+                value[-1] = 'NULL'
+            elif distance > 75 or distance < 0:
+                value[-1] = 'INVALID'
             else:
-                value[-1] = 'INVALID\OUTLIER'
+                value[-1] = 'INVALID'
+        except ValueError:
+            if value[0] in missing:
+                value[-1] = 'NULL'
+            else:
+                value[-1] = 'INVALID'
         return value
 
     def pickup_longitude(output):
@@ -213,15 +230,15 @@ if __name__ == "__main__":
         '''generates output for Fare_amount'''
         try:
             output[0] = float(output[0])
+            if output[0] < 0:
+                output[-1] = 'INVALID'
+            else:
+                output[-1] = 'VALID'
         except:
             if output[0] in missing:
                 output[-1] = 'NULL'
             else:
                 output[-1] = 'INVALID'
-        if output[0] < 0:
-            output[-1] = 'INVALID'
-        else:
-            output[-1] = 'VALID'
         return output
 
     def extra(output, values = [.5, 1.0]):
@@ -306,6 +323,13 @@ if __name__ == "__main__":
         except KeyError:
             pass
 
+    print = print
+
+    globals()['print']('yellow or green ' + str(sys.argv[1]))
+    globals()['print']('column name ' + str(sys.argv[2]))
+    globals()['print']('old_column_number ' + str(old_column_number))
+    globals()['print']('new_column_number ' + str(new_column_number))
+
     if old_column_number >= 0:
         if sys.argv[1] == 'yellow':
             taxi_data_old = sc.textFile('/user/cer446/old_schema/yellow_*.csv')
@@ -318,6 +342,8 @@ if __name__ == "__main__":
         map(getattr(current_module, sys.argv[2].lower())).\
         map(lambda x: str(x[0]) + '\t' + str(x[1]) + '\t' + str(x[2]) + '\t' + str(x[3]))
 
+        globals()['print'](output_old.take(5))
+
     if new_column_number >= 0:
         if sys.argv[1] == 'yellow':
             taxi_data_new = sc.textFile('/user/cer446/new_schema/yellow_*.csv')
@@ -329,6 +355,8 @@ if __name__ == "__main__":
         output_new = output_new.filter(lambda x: x[0].lower() not in column_list).\
         map(getattr(current_module, sys.argv[2].lower())).\
         map(lambda x: str(x[0]) + '\t' + str(x[1]) + '\t' + str(x[2]) + '\t' + str(x[3]))
+
+        globals()['print'](output_new.take(5))
 
     if old_column_number >= 0 and new_column_number >=0:
         results = output_old.union(output_new) #if both results exist, union them
